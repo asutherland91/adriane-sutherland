a71f57d0a2ad7396a44f3316f79c61307435a474
Adriane Sutherland <asutherland91@gmail.com>
2023-10-18 13:38:56 -0700

    Add minor changes to fun about

--- src/Components/FunAbout/FunAbout.js
+++ src/Components/FunAbout/FunAbout.js
@@ -8,17 +8,15 @@ export const FunAbout = () => {
       <h1 className="fun-about-header">How about some fun facts!</h1>
       <div className="fun-about-text-wrapper">
         <p className="fun-about-text">
-          Outside of coding, I enjoy filling my free time with fun hobbies and
-          relaxing activities. You can often find me curled up with a good book,
-          taking leisurely walks around town, playing board games with friends,
+          Outside of coding, I enjoy filling my free time with fun hobbies and activities. 
+          You can often find me curled up with a good book,
+          taking leisurely walks around my city, playing board games with friends,
           or baking up a storm in the kitchen. My baked goods are pretty
-          legendary - I'd be happy to trade cookie recipes anytime!
+          legendary - I'd be happy to trade cookie recipes anytime! I am a huge animal person,
+          I have cats myself, but love pretty much any kind of pet you could imagine!
         </p>
         <p className="fun-about-text">
-          I hope this gives you a snapshot of who I am and what motivates me.
-          Please feel free to connect if you'd ever like to chat more! I'm
-          always open to networking and meeting new people in this fantastic
-          industry.
+          
         </p>
       </div>
     </div>
